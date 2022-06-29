8e32d157828b7b308ea4dd8ff56dd85636e7da0c
KhushiRabadiya <khushirabadiya11@gmail.com>
2022-06-29 13:51:45 +0530

    Ransack gem mvc

--- Gemfile
+++ Gemfile
@@ -77,3 +77,5 @@ gem "devise", "~> 4.8"
 gem "devise_invitable", "~> 2.0"
 
 gem "cancancan", "~> 3.4"
+
+gem "ransack", "~> 3.2"
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
   def index
+    @q = User.ransack(params[:q])
+    @users = @q.result
   end
 end
