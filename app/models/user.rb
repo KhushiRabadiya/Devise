7cdfc0bdc5af598f684c5b9066cbe7fda5b7b5fc
KhushiRabadiya <khushirabadiya11@gmail.com>
2022-06-29 11:57:29 +0530

    Authorization MVC

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :invitable, :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :trackable, :confirmable
+
+  belongs_to :role, foreign_key: "role_id"
+
+  def admin?
+    role.name == 'Admin'
+  end
 end
